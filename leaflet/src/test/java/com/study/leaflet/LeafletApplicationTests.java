468cab8d389bcf0097e29117dad431da119e0f60
Park-Sejin <stsj84@gmail.com>
2023-10-24 17:24:11 +0900

    [2023/10/24] 1. 프로젝트 초기 생성

---
+++ leaflet/src/test/java/com/study/leaflet/LeafletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.leaflet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
